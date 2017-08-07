0e88d017dc31f71edbcfc1952fa09bae76b59b8d
InnovateBham_11 <InnovateBham_11@Admins-iMac.local>
2017-08-07 16:28:04 -0500

    get, pust & post works

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var fs = require('fs');
 var path = require('path');
-var id = require('shortid');
+var ids = require('shortid');
 var shortid = require('shortid');
 var jsonPath = path.join(__dirname, "data.json");
     
@@ -24,59 +24,62 @@ app.get('/chirps', function (req, res) {
         });
 });
 //Checking port
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+
 //Posts
 app.post('/chirps', function (req, res) {
-    console.log('chirp posted')
-     var chunks = '',
-            data;
-
-        req.on('data', function(chunk) {
-            chunks += chunk;
-
-            if (chunks.length > 1e6) {
-                req.connection.destroy();
-            }
-
-            data = JSON.parse(chunks);
-        
+    console.log('posted')    
          fs.readFile(jsonPath, 'utf-8', function(err, file) {
-            if (err) {
-                res.writeHead(500);
-                res.end('Could not read file');
-            }
-
             var arr = JSON.parse(file);
 
-            data.id = shortid.generate();
-
-            arr.push(data);
-
+            req.body.id = shortid.generate();
+            arr.push(req.body);
+            console.log(req.body)
             fs.writeFile(jsonPath, JSON.stringify(arr), function(err, success) {
                 if (err) {
-                    res.writeHead(500);
-                    res.end('Couldn\'t successfull store data');
+                    res.sendStatus(500);
                 } else {
-                    res.writeHead(201, 'Created');
-                    res.end(JSON.stringify(arr));
+                    res.status(201);
+                    res.send(req.body);
                 }
             });
         });
-    });
-});   
+
+});
 
 //Delete Post
 app.delete('/api/chirps/:id', function (req, res) {});
 //Update Post
-app.put('/api/chirps/:id', function (req, res) {
-    var user = req.param.id;
-    var message = req.param.message;
-})
+app.put('/chirps/:id', function (req, res) {
+    console.log('posted')    
+         fs.readFile(jsonPath, 'utf-8', function(err, file) {
+            var arr = JSON.parse(file);
+            var result;
+            var id = req.params.id;
+            arr.forEach(function(a) {
+                if (a.id === id) {
+                    result = a;
+                    result.user = req.body.user;
+                    result.message = req.body.message;
+                }
+            });
+            console.log(req.body)
+            fs.writeFile(jsonPath, JSON.stringify(arr), function(err, success) {
+                if (err) {
+                    res.sendStatus(500);
+                } else {
+                    res.status(201);
+                    res.send(req.body);
+                }
+            });
+        });
+
+});
 
 // router.param('/api/user/', function(req, res, next, user){
 //     console.log('validating users')
 //     req.user = user;
 //     next();
 // })
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
