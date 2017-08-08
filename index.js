9b8c4f89a5791b07642bede1652c2f47796628f0
dnd628 <IamBham2@InnovateBhams-MacBook-Pro.local>
2017-08-07 20:27:27 -0500

    requests working

--- index.js
+++ index.js
@@ -9,77 +9,126 @@ var jsonPath = path.join(__dirname, "data.json");
     
 app.use(bodyParser.json());
 
-//Get Post
-app.get('/chirps', function (req, res) {
-    console.log('Posted')
-   
-     fs.readFile(jsonPath, function(err, file) {
+var jsonPath = path.join(__dirname, 'data.json');
+app.route("/chirps")
+    .get(function(req, res){
+        fs.readFile(jsonPath, function(err, file) {
             if (err) {
                 res.writeHead(500);
-                res.end('Could not read file');
+                res.end('Can not read file');
             }
 
             res.write(file);
             res.end();
         });
-});
-//Checking port
-
-//Posts
-app.post('/chirps', function (req, res) {
-    console.log('posted')    
-         fs.readFile(jsonPath, 'utf-8', function(err, file) {
-            var arr = JSON.parse(file);
-
-            req.body.id = shortid.generate();
-            arr.push(req.body);
-            console.log(req.body)
-            fs.writeFile(jsonPath, JSON.stringify(arr), function(err, success) {
-                if (err) {
-                    res.sendStatus(500);
-                } else {
-                    res.status(201);
-                    res.send(req.body);
-                }
-            });
-        });
-
-});
-
-//Delete Post
-app.delete('/api/chirps/:id', function (req, res) {});
-//Update Post
-app.put('/chirps/:id', function (req, res) {
-    console.log('posted')    
-         fs.readFile(jsonPath, 'utf-8', function(err, file) {
-            var arr = JSON.parse(file);
-            var result;
-            var id = req.params.id;
-            arr.forEach(function(a) {
-                if (a.id === id) {
-                    result = a;
-                    result.user = req.body.user;
-                    result.message = req.body.message;
-                }
-            });
-            console.log(req.body)
-            fs.writeFile(jsonPath, JSON.stringify(arr), function(err, success) {
-                if (err) {
-                    res.sendStatus(500);
-                } else {
-                    res.status(201);
-                    res.send(req.body);
-                }
-            });
-        });
-
-});
-
-// router.param('/api/user/', function(req, res, next, user){
-//     console.log('validating users')
-//     req.user = user;
-//     next();
-// })
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
 })
+    .post(function(req, res){
+        fs.readFile(jsonPath, 'utf-8', function(err, file) {
+            if (err) {
+                res.status(500);
+            } else {
+                var chunks = JSON.parse(file),
+                    chunk = req.body;
+                chunk.id = shortid.generate();
+                chunks.push(chunk);
+                fs.writeFile(jsonPath, JSON.stringify(chunks), function(err, success) {
+                    if (err) {
+                        res.sendStatus(500);
+                    } else {
+                        res.status(201);
+                        res.send(chunk);
+                    }
+                });
+            }
+        });
+    });
+app.route('/chirps/one/:id')
+    .get(function(req, res) {
+        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
+            if (err) {
+                res.statusStatus(500);
+            } else {
+                
+                var chunks = JSON.parse(fileContents);
+            
+                var id = req.params.id;
+            
+                var response;
+
+                chunks.forEach(function(chunk) {
+                    if (chunk.id === id) {
+                        response = chunk;
+                    }
+                });
+                if (response) {
+                    res.send(response);
+                } else {
+                    res.sendStatus(404);
+                }
+            }
+        });
+    })
+    .put(function(req, res) {
+        fs.readFile(jsonPath, 'utf-8', function(err, file) {
+            if (err) {
+                res.statusStatus(500);
+            } else {
+                var arr = JSON.parse(file);
+
+                var response;
+
+                var id = req.params.id;
+                
+                arr.forEach(function(a) {
+                    if (a.id === id) {
+                        response = a;
+                        response.user = req.body.user;
+                        response.chirp= req.body.chirp;
+                    }
+                });
+            fs.writeFile(jsonPath, JSON.stringify(arr), function(err, success) {
+                if (err) {
+                    res.sendStatus(500);
+                } else {
+                    res.status(201);
+                    res.send(req.body);
+                }
+            });
+        }
+        });
+    })
+  
+    .delete(function(req, res) {
+        fs.readFile(jsonPath, 'utf-8', function(err, fileContents) {
+            if (err) {
+                res.sendStatus(500);
+            } else {
+                var chunks = JSON.parse(fileContents);
+                var id = req.params.id;
+                var deleteIndex = -1;
+                chunks.forEach(function(chunk, i) {
+                    if (chunk.id === id) {
+                        deleteIndex = i;
+                    }
+                });
+                if (deleteIndex != -1) {
+                    chunks.splice(deleteIndex, 1);
+                    fs.writeFile(jsonPath, JSON.stringify(chunks), function(err, success) {
+                        if (err) {
+                            res.sendStatus(500);
+                        } else {
+                            res.sendStatus(202);
+                        }
+                    });
+                } else {
+                    res.sendStatus(404);
+                }
+            }
+        });
+    });
+        
+        
+    
+app.listen(3000, function(){
+    console.log("listening on port 3000!");
+});
